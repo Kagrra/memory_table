daf06434317a02802d5e536a03a88837425eba80
michal.andrasiak <michal.andrasiak@gmail.com>
2022-01-31 13:53:23 +0100

    Add label generation for supported types

--- include/kgr/memory_table.h
+++ include/kgr/memory_table.h
@@ -53,6 +53,104 @@ template<typename AddressType, typename... Ts> struct memory_table
     template<typename T> constexpr static std::size_t width_of_v = width_of<T>::value;
 
     constexpr static std::size_t line_width = address_width + (width_of<Ts>::value + ...) + sizeof...(Ts) + 2;
+
+    // returns array with label in center
+    template<int I> static constexpr std::array<char, I> in_center(const char* label)
+    {
+        auto strlen = [](const char* str) constexpr->size_t
+        {
+            size_t i = 0;
+            while (str[++i]) { }
+            return i;
+        };
+
+        std::array<char, I> arr {};
+        arr.fill(' ');
+        const int margin = (arr.size() - strlen(label)) / 2;
+
+        for (int i = margin; i < margin + strlen(label); ++i) { arr[i] = label[i - margin]; }
+        return arr;
+    }
+
+    template<typename T> static constexpr auto label()
+    {
+        if constexpr (std::is_same_v<T, float> || std::is_same_v<T, double>)
+        {
+            return in_center<width_of_v<float>>("float");
+        }
+
+        // here we want numbered bits
+        if constexpr (std::is_same_v<T, bool>)
+        {
+            // becouse constexpr
+            std::array<std::array<char, 2>, 8> bool_label
+                = {{{' ', '0'}, {'7', ' '}, {' ', '8'}, {'1', '5'}, {'1', '6'}, {'2', '3'}, {'2', '4'}, {'3', '1'}}};
+            std::array<char, width_of_v<bool>> arr;
+            arr.fill(' ');
+            int bool_state = 0;
+            int bool_label_num = 0;
+            for (int i = arr.size() - 1; i >= 0; i--)
+            {
+                switch (bool_state)
+                {
+                    case 0:
+                    {
+                        // space at the beginning of byte
+                        bool_state = 1;
+                        break;
+                    }
+                    case 1:
+                    {
+                        arr[i] = bool_label[bool_label_num][1];
+                        bool_state++;
+                        break;
+                    }
+                    case 2:
+                    {
+                        arr[i] = bool_label[bool_label_num][0];
+                        bool_label_num++;
+                        bool_state++;
+                        break;
+                    }
+                    case 7:
+                    {
+                        arr[i] = bool_label[bool_label_num][1];
+                        bool_state++;
+                        break;
+                    }
+                    case 8:
+                    {
+                        arr[i] = bool_label[bool_label_num][0];
+                        bool_label_num++;
+                        bool_state = 0;
+                        break;
+                    }
+                    default:
+                    {
+                        bool_state++;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        if constexpr (std::is_same_v<T, char>)
+        {
+            return in_center<width_of_v<char>>("char");
+        }
+
+        if constexpr (std::is_same_v<T, uint8_t>)
+        {
+            return in_center<width_of_v<T>>("hex");
+        }
+
+        if constexpr (std::is_same_v<T, uint16_t> || std::is_same_v<T, uint32_t> //
+                      || std::is_same_v<T, int16_t> || std::is_same_v<T, int32_t> //
+                      || std::is_same_v<T, uint64_t> || std::is_same_v<T, int64_t>)
+        {
+            return in_center<width_of_v<T>>("dec");
+        }
+    };
 };
 
 } // namespace kgr
