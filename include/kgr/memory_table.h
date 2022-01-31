7aab94eb74bf4f37e7541b632dd032bc9a0908d6
michal.andrasiak <michal.andrasiak@gmail.com>
2022-01-31 14:34:43 +0100

    Reorganize code

--- include/kgr/memory_table.h
+++ include/kgr/memory_table.h
@@ -2,7 +2,10 @@
 #define KGR_MEMORY_TABLE_H
 
 #include <array>
+#include <bitset>
 #include <cstdint>
+#include <iomanip>
+#include <iostream>
 #include <string_view>
 #include <type_traits>
 
@@ -10,15 +13,10 @@ namespace kgr
 {
 template<typename AddressType, typename... Ts> class memory_table
 {
-public:
-    constexpr static std::size_t table_width = address_width + (width_of<Ts>::value + ...) + sizeof...(Ts) + 2;
-    constexpr static std::string_view header {header_as_array.data(), header_as_array.size()};
-
 private:
     constexpr static std::size_t margin_width = 2;
     constexpr static std::size_t bytes = std::max({sizeof(Ts)...});
-    constexpr static std::size_t address_width = 2 /* 0x */ + sizeof(AddressType) * 2 + margin_size;
-
+    constexpr static std::size_t address_width = 2 /* 0x */ + sizeof(AddressType) * 2 + margin_width;
     // space for each field
     // 8 characters for floating types
     // 8*bytes for bool with spaces between bytes
@@ -58,6 +56,8 @@ private:
     };
 
     template<typename T> constexpr static std::size_t width_of_v = width_of<T>::value;
+
+    constexpr static std::size_t table_width_ = address_width + (width_of_v<Ts> + ...) + sizeof...(Ts) + 2;
 
     // returns array with label in center
     template<int I> static constexpr std::array<char, I> in_center(const char* label)
@@ -158,10 +158,10 @@ private:
         }
     };
 
-    constexpr static std::array<char, table_width> header_as_array = []() constexpr
+    constexpr static std::array<char, table_width_> header_as_array_ = []() constexpr
     {
         std::array<char, 1> divider {'|'};
-        std::array<char, table_width> arr;
+        std::array<char, table_width_> arr;
 
         auto ptr = arr.begin();
 
@@ -170,12 +170,60 @@ private:
             ptr = std::copy(txt.begin(), txt.end(), ptr);
         };
 
-        concat(in_center<address_size>("addr"));
+        concat(in_center<address_width>("addr"));
         (concat(label<Ts>()), ...);
         ptr = std::copy(divider.begin(), divider.end(), ptr);
         return arr;
     }
     ();
+
+    template<typename T> auto print(const std::array<uint8_t, bytes>& data) -> void
+    {
+        if constexpr (std::is_same_v<T, float> || std::is_same_v<T, double>)
+        {
+            T temp;
+            memcpy(&temp, data.data(), sizeof(T));
+            std::cout << std::setw(8) << temp << "|";
+        }
+
+        if constexpr (std::is_same_v<T, bool>)
+        {
+            std::cout << " ";
+            for (int i = bytes - 1; i >= 0; i--)
+            {
+                std::bitset<8> x(data[i]);
+                std::cout << x << ' ';
+            }
+            std::cout << "|";
+        }
+
+        if constexpr (std::is_same_v<T, uint16_t> || std::is_same_v<T, uint32_t> //
+                      || std::is_same_v<T, int16_t> || std::is_same_v<T, int32_t> //
+                      || std::is_same_v<T, uint64_t> || std::is_same_v<T, int64_t>)
+        {
+            T temp {};
+            memcpy(&temp, data.data(), sizeof(T));
+            std::cout << " " << std::dec << std::setw(width_of_v<T> - margin_width) << temp << " |";
+        }
+
+        if constexpr (std::is_same_v<T, char>)
+        {
+            std::cout << " ";
+            for (int i = bytes - 1; i >= 0; i--) { std::cout << (char)data[i] << " "; }
+            std::cout << "|";
+        }
+
+        if constexpr (std::is_same_v<T, uint8_t>)
+        {
+            std::cout << " ";
+            for (int i = bytes - 1; i >= 0; i--) std::cout << std::setw(2) << std::hex << (int)data[i] << " ";
+            std::cout << "|";
+        }
+    };
+
+public:
+    constexpr static std::string_view header {header_as_array_.data(), header_as_array_.size()};
+    constexpr static std::size_t line_width {table_width_};
 };
 
 } // namespace kgr
